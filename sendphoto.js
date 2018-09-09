74fef5ad95dbe1eb107ff5ecc23adcf6f480d289
Agatha Turyahikayo <agathaturyahikayo14@gmail.com>
2018-09-09 04:02:32 -0400

    Update sendphoto.js

--- sendphoto.js
+++ sendphoto.js
@@ -24,7 +24,7 @@ document.getElementById("capture").onclick = function() {
         var xhr = new XMLHttpRequest();
         
         var url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v2.0/Prediction/1b2c0a21-2177-4299-aa3f-fe218235cdc4/url";
-        xhr.open("POST", url, true);
+        xhr.open("POST", data, true);
         xhr.setRequestHeader("Content-Type", "application/json");
 
         xhr.setRequestHeader("Prediction-Key", "e468e9927724421ea6995c7293a13788");
