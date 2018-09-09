c46748634bef62691b7ba6796b54d802a46226a4
Agatha Turyahikayo <agathaturyahikayo14@gmail.com>
2018-09-09 07:48:32 -0400

    Update sendphoto.js

--- sendphoto.js
+++ sendphoto.js
@@ -32,7 +32,8 @@ document.getElementById("capture").onclick = function() {
         xhr.onreadystatechange = function () {
             if (xhr.readyState === 4 && xhr.status === 200) {
 
-                console.log(xhr.responseText);
+                //console.log(xhr.responseText);
+                console.log(JSON.parse(xhr.responseText));
                 process(JSON.parse(xhr.responseText));
 
             }
