ecba5efe035ee409c6951b7bdbca1f943df76719
Agatha Turyahikayo <agathaturyahikayo14@gmail.com>
2018-09-09 06:45:25 -0400

    Update sendphoto.js

--- sendphoto.js
+++ sendphoto.js
@@ -21,7 +21,7 @@ document.getElementById("capture").onclick = function() {
         document.getElementById("webcam").hidden = true;
 
         // Sending and receiving data in JSON format using POST method
-
+        alert('HEYEYEYE')
         var xhr = new XMLHttpRequest();
         var url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v2.0/Prediction/1b2c0a21-2177-4299-aa3f-fe218235cdc4/image/";
          xhr.open("POST", url, true);
@@ -49,10 +49,29 @@ document.getElementById("capture").onclick = function() {
         };
 
         //var data = JSON.stringify({"Url": "https://melanotix.me/capture.png"});
-        var body = img.src;
+        var body = dataURItoBlob(img.src);
         xhr.send(body);
 
         
 
 };
+function dataURItoBlob(dataURI) {
+    // convert base64/URLEncoded data component to raw binary data held in a string
+    var byteString;
+    if (dataURI.split(',')[0].indexOf('base64') >= 0)
+        byteString = atob(dataURI.split(',')[1]);
+    else
+        byteString = unescape(dataURI.split(',')[1]);
+
+    // separate out the mime component
+    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+    // write the bytes of the string to a typed array
+    var ia = new Uint8Array(byteString.length);
+    for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+    }
+
+    return new Blob([ia], {type:mimeString});
+}
 
